68d9c0837caef44089c27e72291801527ce7f40b
Jason <jason@jason.dev.tt>
2019-07-26 14:00:33 +0800

    上传文件修改，订单企业信息pdf下载,打印功能222

--- src/utils/request.js
+++ src/utils/request.js
@@ -35,7 +35,7 @@ if (window.location.origin.indexOf('dev.newdreamservices.com') != -1) {
 const service = axios.create({
   // baseURL: process.env.NODE_ENV === 'development'?'api/':'https://dev.newdreamservices.com', // url = base url + request url
   baseURL: process.env.NODE_ENV === 'development' ? 'api/' : serverUrl, // url = base url + request url
-  timeout: 30000 // request timeout
+  timeout: 300000 // request timeout
 });
 // request interceptor
 service.interceptors.request.use(
